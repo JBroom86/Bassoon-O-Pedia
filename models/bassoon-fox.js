725582b42b4d752da1456500643fdc051d198d88
jbroom86 <jbroom86@gmail.com>
2023-03-11 16:15:19 -0800

    Added Fox Bassoon Schema

---
+++ models/bassoon-fox.js
@@ -0,0 +1,14 @@
+// Requiring the Mongoose package
+const mongoose = require('mongoose');
+
+// Fox Bassoon Schema
+const bassoonFoxSchema = new mongoose.Schema({
+    make: { type: String, required: true },
+    model: { type: String, required: true },
+    details: { type: String, required: true},
+    serialNumber: { type: Number, required: true},
+    history: { type: String }
+});
+
+// Exporting the schema as a Mongoose model
+module.exports = mongoose.model('Fox', bassoonFoxSchema);
--- models/bassoons.js
+++
@@ -1,9 +0,0 @@
-// Requiring the Mongoose package
-const mongoose = require('mongoose');
-
-const bassoonSchema = new mongoose.Schema({
-    make: { type: String, required: true },
-    model: { type: String, required: true },
-    details: { type: String, required: true},
-    
-})
